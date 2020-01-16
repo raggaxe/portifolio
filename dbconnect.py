22853632aa777f98303b1ba5f0c1ef9af4815175
Rafael Almeida <raggaxe@gmail.com>
2020-01-16 22:05:09 +0000

    app changes

--- dbconnect.py
+++ dbconnect.py
@@ -1,9 +1,9 @@
 import pymysql
 
 def connection():
-    conn = pymysql.connect(host="localhost",
-                           user = 'root',
-                           passwd = 'rootpass',
-                           db = 'portifolio')
+    conn = pymysql.connect(host="us-cdbr-iron-east-05.cleardb.net",
+                           user = 'b855d419d92593',
+                           passwd = 'b8bb879b',
+                           db = 'heroku_4a2f43692cff6c6')
     c = conn.cursor()
     return c,conn
