65e426de7164713a314036e1268992a8aab88140
Rafael Almeida <raggaxe@gmail.com>
2024-06-18 11:32:06 -0300

    Update dbconnect.py

--- dbconnect.py
+++ dbconnect.py
@@ -2,8 +2,8 @@ import pymysql
 
 def connection():
     conn = pymysql.connect(host="us-cdbr-iron-east-05.cleardb.net",
-                           user = 'b855d419d92593',
-                           passwd = 'b8bb879b',
-                           db = 'heroku_4a2f43692cff6c6')
+                           user = '',
+                           passwd = '',
+                           db = '')
     c = conn.cursor()
     return c,conn
